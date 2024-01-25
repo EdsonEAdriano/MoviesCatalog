4c8bfbd211a73abd97c687892771a91cbf0341f9
Edsu <edsoneurides0705@gmail.com>
2024-01-24 22:29:30 -0300

    Classe para gerar exceções

---
+++ MoviesCatalog.Domain/Validation/DomainExceptionValidation.cs
@@ -0,0 +1,14 @@
+﻿namespace MoviesCatalog.Domain.Validation;
+
+public class DomainExceptionValidation : Exception
+{
+    public DomainExceptionValidation(string? message) : base(message)
+    {
+    }
+
+    public static void When(bool hasError, string error)
+    {
+        if (hasError)
+            throw new DomainExceptionValidation(error);
+    }
+}
