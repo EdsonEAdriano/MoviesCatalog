2aafcba01a807b7784c323892f2dd757bd1b3c88
Edsu <edsoneurides0705@gmail.com>
2024-01-24 22:39:32 -0300

    Criação da interface base para repositórios

---
+++ MoviesCatalog.Domain/Interfaces/IBaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using MoviesCatalog.Domain.Entities;
+
+namespace MoviesCatalog.Domain.Interfaces;
+
+public interface IBaseRepository<T> where T : BaseEntity
+{
+    Task<IEnumerable<T>> GetAsync();
+    Task<T> GetAsync(int? id);
+
+    Task<T> CreateAsync(T entity);
+    Task<T> UpdateAsync(T entity);
+    Task<T> RemoveAsync(T entity);
+}
