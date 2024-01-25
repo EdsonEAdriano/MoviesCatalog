d74c032b9b16edc85180df8edf8cc069c2f12da8
Edsu <edsoneurides0705@gmail.com>
2024-01-24 22:28:42 -0300

    Criação de entidade base contendo a propriedade Id

---
+++ MoviesCatalog.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿using MoviesCatalog.Domain.Validation;
+
+namespace MoviesCatalog.Domain.Entities;
+
+public abstract class BaseEntity
+{
+    public int Id { get; protected set; }
+
+    public BaseEntity()
+    {
+    }
+    
+    public BaseEntity(int id)
+    {
+        DomainExceptionValidation.When(id < 0,
+            "Invalid Id value");
+
+        Id = id;
+    }
+}
