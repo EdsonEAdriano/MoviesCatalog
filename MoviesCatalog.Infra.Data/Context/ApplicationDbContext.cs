760d3fb02d998d165ddaca8fea6f29c8076ac80e
Edsu <edsoneurides0705@gmail.com>
2024-01-25 22:54:53 -0300

    Criação do contexto do banco

---
+++ MoviesCatalog.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoviesCatalog.Domain.Entities;
+
+namespace MoviesCatalog.Infra.Data.Context;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    { }
+    
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Movie> Movies { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+}
