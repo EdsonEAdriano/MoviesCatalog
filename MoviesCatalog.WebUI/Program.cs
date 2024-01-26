693f0da9b6b39999e24f5825bccfcfebd5764486
Edsu <edsoneurides0705@gmail.com>
2024-01-25 23:02:26 -0300

    Injetando dependências

--- MoviesCatalog.WebUI/Program.cs
+++ MoviesCatalog.WebUI/Program.cs
@@ -1,7 +1,13 @@
+using MoviesCatalog.Infra.IoC;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddInfrastructure(builder.Configuration);
+
+
 
 var app = builder.Build();
 
