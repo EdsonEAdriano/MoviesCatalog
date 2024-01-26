4ca17d4d645011ba83ac9817ff7aa4acec0a0ad5
Edsu <edsoneurides0705@gmail.com>
2024-01-25 22:57:38 -0300

    repositório de filme

---
+++ MoviesCatalog.Infra.Data/Repositories/MovieRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoviesCatalog.Domain.Entities;
+using MoviesCatalog.Domain.Interfaces;
+using MoviesCatalog.Infra.Data.Context;
+
+namespace MoviesCatalog.Infra.Data.Repositories;
+
+public class MovieRepository : BaseRepository<Movie>, IMovieRepository
+{
+    public MovieRepository(ApplicationDbContext context) : base(context)
+    { }
+    
+    public async Task<Movie> GetAsync(Category category)
+    {
+        return await _context
+            .Movies
+            .Include(m => m.Category)
+            .SingleOrDefaultAsync(m => m.Id == category.Id);
+    }
+}
