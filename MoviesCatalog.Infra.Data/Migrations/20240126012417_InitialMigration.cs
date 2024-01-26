4b4c4de0a52bf5c63ab1345949d661a1ffa6475b
Edsu <edsoneurides0705@gmail.com>
2024-01-25 23:04:40 -0300

    Inserindo migrações

---
+++ MoviesCatalog.Infra.Data/Migrations/20240126012417_InitialMigration.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MoviesCatalog.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    ReleaseDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    ImagePath = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CategoryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Movies_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Ficção" },
+                    { 2, "Suspense" },
+                    { 3, "Terror" },
+                    { 4, "Comédia" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movies_CategoryId",
+                table: "Movies",
+                column: "CategoryId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movies");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
---
+++ MoviesCatalog.Infra.Data/Migrations/20240126013206_SeedMovies.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MoviesCatalog.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedMovies : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("INSERT INTO Movies(Title, Description,ReleaseDate, ImagePath, CategoryId) " +
+                   "VALUES('The Conjuring: Last Rites', 'The Conjuring: Last Rites is the next chapter in the successful horror franchise, following the adventures of paranormal experts Ed and Lorraine Warren.', '2025-01-01', 'TheConjuring.jpg', 3)");
+        
+            mb.Sql("INSERT INTO Movies(Title, Description, ReleaseDate, ImagePath, CategoryId) " +
+                   "VALUES('Interstellar', 'A viagem interestelar de uma equipe de exploradores em busca de um novo lar para a humanidade.', '2014-11-07', 'Interstellar.jpg', 1)");
+            
+            mb.Sql("INSERT INTO Movies(Title, Description, ReleaseDate, ImagePath, CategoryId) " +
+                   "VALUES('Gone Girl', 'Um suspense psicológico sobre o desaparecimento misterioso de uma mulher.', '2014-10-03', 'GoneGirl.jpg', 2)");
+            
+            mb.Sql("INSERT INTO Movies(Title, Description, ReleaseDate, ImagePath, CategoryId) " +
+                   "VALUES('Superbad', 'Duas amigos tentam fazer a festa de formatura épica antes de se formarem no ensino médio.', '2007-08-17', 'Superbad.jpg', 4)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("DELETE Movies");
+        }
+    }
+}
