a64878c70ef8731d587e62070d3399a7b22681ce
Edsu <edsoneurides0705@gmail.com>
2024-01-25 22:57:29 -0300

    repositório de categoria

---
+++ MoviesCatalog.Infra.Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using MoviesCatalog.Domain.Entities;
+using MoviesCatalog.Domain.Interfaces;
+using MoviesCatalog.Infra.Data.Context;
+
+namespace MoviesCatalog.Infra.Data.Repositories;
+
+public class CategoryRepository : BaseRepository<Category>, ICategoryRepository
+{
+    public CategoryRepository(ApplicationDbContext context) : base(context)
+    { }
+
+}
