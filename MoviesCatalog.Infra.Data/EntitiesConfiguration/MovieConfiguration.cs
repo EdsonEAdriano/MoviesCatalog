c633876cff9964fc39f5fa9b034c4c7f19c207b9
Edsu <edsoneurides0705@gmail.com>
2024-01-25 22:55:50 -0300

    Configuração da entidade filme no banco

---
+++ MoviesCatalog.Infra.Data/EntitiesConfiguration/MovieConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MoviesCatalog.Domain.Entities;
+
+namespace MoviesCatalog.Infra.Data.EntitiesConfiguration;
+
+public class MovieConfiguration : IEntityTypeConfiguration<Movie>
+{
+    public void Configure(EntityTypeBuilder<Movie> builder)
+    {
+        builder.HasKey(m => m.Id);
+        builder.Property(m => m.Title).HasMaxLength(100).IsRequired();
+        builder.Property(m => m.Description).HasMaxLength(200).IsRequired();
+        
+        builder.Property(m => m.ReleaseDate).IsRequired();
+
+        builder.HasOne(m => m.Category)
+            .WithMany(c => c.Movies)
+            .HasForeignKey(m => m.CategoryId);
+    }
+}
