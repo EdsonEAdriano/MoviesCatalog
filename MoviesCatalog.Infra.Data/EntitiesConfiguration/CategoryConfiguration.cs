5f91c46f3c38687743507f3358bd32ecd533faea
Edsu <edsoneurides0705@gmail.com>
2024-01-25 22:55:33 -0300

    Configuração das entidade categoria do banco

---
+++ MoviesCatalog.Infra.Data/EntitiesConfiguration/CategoryConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MoviesCatalog.Domain.Entities;
+
+namespace MoviesCatalog.Infra.Data.EntitiesConfiguration;
+
+public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.HasKey(c => c.Id);
+        builder.Property(c => c.Name).HasMaxLength(100).IsRequired();
+
+        builder.HasData(
+            new Category(1, "Ficção"),
+            new Category(2, "Suspense"),
+            new Category(3, "Terror"),
+            new Category(4, "Comédia")
+        );
+    }
+}
