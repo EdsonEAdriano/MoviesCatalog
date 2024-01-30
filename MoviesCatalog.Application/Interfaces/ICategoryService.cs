920cdfc9d8382ca685d635cb13d0258508d599ce
Edsu <edsoneurides0705@gmail.com>
2024-01-30 20:30:51 -0300

    Criação das interfaces de serviços

---
+++ MoviesCatalog.Application/Interfaces/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using MoviesCatalog.Application.DTOs;
+
+namespace MoviesCatalog.Application.Interfaces;
+
+public interface ICategoryService
+{
+    Task<IEnumerable<CategoryDTO>> GetAsync();
+    Task<CategoryDTO> GetAsync(int? id);
+
+    Task CreateAsync(CategoryDTO categoryDTO);
+    Task UpdateAsync(CategoryDTO categoryDTO);
+    Task RemoveAsync(int? id);
+}
---
+++ MoviesCatalog.Application/Interfaces/IMovieService.cs
@@ -0,0 +1,14 @@
+﻿using MoviesCatalog.Application.DTOs;
+
+namespace MoviesCatalog.Application.Interfaces;
+
+public interface IMovieService
+{
+    Task<IEnumerable<MovieDTO>> GetAsync();
+    Task<MovieDTO> GetAsync(int? id);
+    Task<MovieDTO> GetMovieCategoryAsync(int? id);
+
+    Task CreateAsync(MovieDTO movieDTO);
+    Task UpdateAsync(MovieDTO movieDTO);
+    Task RemoveAsync(int? id);
+}
