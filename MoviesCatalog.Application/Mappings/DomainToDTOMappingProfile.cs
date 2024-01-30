4a53f57cfcd5fc1b0f9d96c0612d7ff4376fac4b
Edsu <edsoneurides0705@gmail.com>
2024-01-30 20:27:17 -0300

    Criação do mapeamento entre entidades

---
+++ MoviesCatalog.Application/Mappings/DomainToDTOMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using MoviesCatalog.Application.DTOs;
+using MoviesCatalog.Domain.Entities;
+
+namespace MoviesCatalog.Application.Mappings;
+
+public class DomainToDTOMappingProfile : Profile
+{
+    public DomainToDTOMappingProfile()
+    {
+        CreateMap<Category, CategoryDTO>().ReverseMap();
+        CreateMap<Movie, MovieDTO>().ReverseMap();
+    }
+}
