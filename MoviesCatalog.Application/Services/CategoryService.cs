8aae42a420aaaebd655526f61c6ce4ec886ec049
Edsu <edsoneurides0705@gmail.com>
2024-01-30 20:31:37 -0300

    Implementação dos serviços

---
+++ MoviesCatalog.Application/Services/CategoryService.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using MoviesCatalog.Application.DTOs;
+using MoviesCatalog.Application.Interfaces;
+using MoviesCatalog.Domain.Entities;
+using MoviesCatalog.Domain.Interfaces;
+
+namespace MoviesCatalog.Application.Services;
+
+public class CategoryService : ICategoryService
+{
+    private readonly ICategoryRepository _categoryRepository;
+    private readonly IMapper _mapper;
+
+    public CategoryService(ICategoryRepository categoryRepository, IMapper mapper)
+    {
+        _categoryRepository = categoryRepository ??
+                              throw new ArgumentNullException(nameof(categoryRepository));;
+        _mapper = mapper;
+    }
+
+    public async Task<IEnumerable<CategoryDTO>> GetAsync()
+    {
+        var categories = await _categoryRepository.GetAsync();
+
+        return _mapper.Map<IEnumerable<CategoryDTO>>(categories);
+    }
+
+    public async Task<CategoryDTO> GetAsync(int? id)
+    {
+        var category = await _categoryRepository.GetAsync(id);
+        
+        return _mapper.Map<CategoryDTO>(category);
+    }
+
+    public async Task CreateAsync(CategoryDTO categoryDTO)
+    {
+        var category = _mapper.Map<Category>(categoryDTO);
+
+        await _categoryRepository.CreateAsync(category);
+    }
+
+    public async Task UpdateAsync(CategoryDTO categoryDTO)
+    {
+        var category = _mapper.Map<Category>(categoryDTO);
+
+        await _categoryRepository.UpdateAsync(category);
+    }
+
+    public async Task RemoveAsync(int? id)
+    {
+        var category = _categoryRepository.GetAsync(id).Result;
+
+        await _categoryRepository.RemoveAsync(category);
+    }
+}
---
+++ MoviesCatalog.Application/Services/MovieService.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using MoviesCatalog.Application.DTOs;
+using MoviesCatalog.Application.Interfaces;
+using MoviesCatalog.Domain.Entities;
+using MoviesCatalog.Domain.Interfaces;
+
+namespace MoviesCatalog.Application.Services;
+
+public class MovieService : IMovieService
+{
+    private readonly IMovieRepository _movieRepository;
+    private readonly IMapper _mapper;
+
+    public MovieService(IMovieRepository movieRepository, IMapper mapper)
+    {
+        _movieRepository = movieRepository ??
+                            throw new ArgumentNullException(nameof(movieRepository));
+        
+        _mapper = mapper;
+    }
+
+    public async Task<IEnumerable<MovieDTO>> GetAsync()
+    {
+        var categories = await _movieRepository.GetAsync();
+
+        return _mapper.Map<IEnumerable<MovieDTO>>(categories);
+    }
+
+    public async Task<MovieDTO> GetAsync(int? id)
+    {
+        var movie = await _movieRepository.GetAsync(id);
+        
+        return _mapper.Map<MovieDTO>(movie);
+    }
+
+    public async Task<MovieDTO> GetMovieCategoryAsync(int? id)
+    {
+        var movie = await _movieRepository.GetMovieCategoryAsync(id);
+        
+        return _mapper.Map<MovieDTO>(movie);
+    }
+
+    public async Task CreateAsync(MovieDTO movieDTO)
+    {
+        var movie = _mapper.Map<Movie>(movieDTO);
+
+        await _movieRepository.CreateAsync(movie);
+    }
+
+    public async Task UpdateAsync(MovieDTO movieDTO)
+    {
+        var movie = _mapper.Map<Movie>(movieDTO);
+
+        await _movieRepository.UpdateAsync(movie);
+    }
+
+    public async Task RemoveAsync(int? id)
+    {
+        var movie = _movieRepository.GetAsync(id).Result;
+
+        await _movieRepository.RemoveAsync(movie);
+    }
+}
