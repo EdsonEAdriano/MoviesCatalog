959a0ef8017e46a634ff7f9bcc6388f8bd9bf09a
Edsu <edsoneurides0705@gmail.com>
2024-01-30 22:19:15 -0300

    Criado mapeamento para Modelos de comandos

---
+++ MoviesCatalog.Application/Mappings/DTOToCommandMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using MoviesCatalog.Application.DTOs;
+using MoviesCatalog.Application.Movies.Commands;
+
+namespace MoviesCatalog.Application.Mappings;
+
+public class DTOToCommandMappingProfile : Profile
+{
+    public DTOToCommandMappingProfile()
+    {
+        CreateMap<MovieDTO, MovieCreateCommand>();
+        CreateMap<MovieDTO, MovieUpdateCommand>();
+    }
+}
