f5304914d5acc285b354bca6d44e09ec4268c934
Edsu <edsoneurides0705@gmail.com>
2024-01-30 22:25:03 -0300

    Implementação da interface de serviços de filmes usando o mediatR

--- MoviesCatalog.Application/Services/MovieService.cs
+++ MoviesCatalog.Application/Services/MovieService.cs
@@ -1,6 +1,9 @@
 ﻿using AutoMapper;
+using MediatR;
 using MoviesCatalog.Application.DTOs;
 using MoviesCatalog.Application.Interfaces;
+using MoviesCatalog.Application.Movies.Commands;
+using MoviesCatalog.Application.Movies.Queries;
 using MoviesCatalog.Domain.Entities;
 using MoviesCatalog.Domain.Interfaces;
 
@@ -8,56 +11,54 @@ namespace MoviesCatalog.Application.Services;
 
 public class MovieService : IMovieService
 {
-    private readonly IMovieRepository _movieRepository;
+    private readonly IMediator _mediator;
     private readonly IMapper _mapper;
 
-    public MovieService(IMovieRepository movieRepository, IMapper mapper)
+    public MovieService(IMediator mediator, IMapper mapper)
     {
-        _movieRepository = movieRepository ??
-                            throw new ArgumentNullException(nameof(movieRepository));
-        
+        _mediator = mediator;
         _mapper = mapper;
     }
 
     public async Task<IEnumerable<MovieDTO>> GetAsync()
     {
-        var categories = await _movieRepository.GetAsync();
+        var query = new GetMoviesQuery();
 
-        return _mapper.Map<IEnumerable<MovieDTO>>(categories);
+        if (query == null)
+            throw new Exception("Entity could not be loaded.");
+        
+        var result = await _mediator.Send(query);
+        
+        return _mapper.Map<IEnumerable<MovieDTO>>(result);
     }
 
     public async Task<MovieDTO> GetAsync(int? id)
     {
-        var movie = await _movieRepository.GetAsync(id);
+        var query = new GetMovieByIdQuery(id.Value);
         
-        return _mapper.Map<MovieDTO>(movie);
-    }
+        if (query == null)
+            throw new Exception("Entity could not be loaded.");
 
-    public async Task<MovieDTO> GetMovieCategoryAsync(int? id)
-    {
-        var movie = await _movieRepository.GetMovieCategoryAsync(id);
-        
-        return _mapper.Map<MovieDTO>(movie);
-    }
+        var result = await _mediator.Send(query);
 
+        return _mapper.Map<MovieDTO>(result);
+    }
+    
     public async Task CreateAsync(MovieDTO movieDTO)
     {
-        var movie = _mapper.Map<Movie>(movieDTO);
-
-        await _movieRepository.CreateAsync(movie);
+        var query = _mapper.Map<MovieCreateCommand>(movieDTO);
+        await _mediator.Send(query);
     }
-
+    
     public async Task UpdateAsync(MovieDTO movieDTO)
     {
-        var movie = _mapper.Map<Movie>(movieDTO);
-
-        await _movieRepository.UpdateAsync(movie);
+        var query = _mapper.Map<MovieUpdateCommand>(movieDTO);
+        await _mediator.Send(query);
     }
-
+    
     public async Task RemoveAsync(int? id)
     {
-        var movie = _movieRepository.GetAsync(id).Result;
-
-        await _movieRepository.RemoveAsync(movie);
+        var query = new MovieRemoveCommand(id.Value);
+        await _mediator.Send(query);
     }
 }
