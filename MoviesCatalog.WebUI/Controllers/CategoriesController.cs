fdd25ee97333136d27efcfb6e5a78f42a11d8e9d
Edsu <edsoneurides0705@gmail.com>
2024-02-05 19:58:21 -0300

    Implementação do controlador de categorias

--- MoviesCatalog.WebUI/Controllers/CategoriesController.cs
+++ MoviesCatalog.WebUI/Controllers/CategoriesController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MoviesCatalog.Application.DTOs;
 using MoviesCatalog.Application.Interfaces;
 
 namespace MoviesCatalog.WebUI.Controllers;
@@ -18,5 +19,102 @@ public class CategoriesController : Controller
     {
         var categories = await _categoryService.GetAsync();
         return View(categories);
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> Create(CategoryDTO category)
+    {
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                await _categoryService.CreateAsync(category);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            
+            return RedirectToAction(nameof(Index));
+        }
+        
+        return View(category);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null)
+            return NotFound();
+
+        var category = await _categoryService.GetAsync(id);
+
+        if (category == null)
+            return NotFound();
+        
+        return View(category);
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> Edit(CategoryDTO category)
+    {
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                await _categoryService.UpdateAsync(category);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        return View(category);
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null)
+            return NotFound();
+
+        var category = await _categoryService.GetAsync(id);
+
+        if (category == null)
+            return NotFound();
+        
+        return View(category);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public async Task<IActionResult> DeleteConfirmed(int? id)
+    {
+        await _categoryService.RemoveAsync(id);
+        return RedirectToAction("Index");
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null)
+            return NotFound();
+
+        var category = await _categoryService.GetAsync(id);
+
+        if (category == null)
+            return NotFound();
+        
+        return View(category);
     }
 }
