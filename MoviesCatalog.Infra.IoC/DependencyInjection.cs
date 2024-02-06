45811f7e41a05d8f8e9b82c0fdaae171ff8a344f
Edsu <edsoneurides0705@gmail.com>
2024-02-05 21:19:35 -0300

    Adicionado as dependencias para segurança

--- MoviesCatalog.Infra.IoC/DependencyInjection.cs
+++ MoviesCatalog.Infra.IoC/DependencyInjection.cs
@@ -1,12 +1,15 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using MoviesCatalog.Application.Interfaces;
 using MoviesCatalog.Application.Mappings;
 using MoviesCatalog.Application.Services;
+using MoviesCatalog.Domain.Account;
 using MoviesCatalog.Domain.Interfaces;
 using MoviesCatalog.Infra.Data.Context;
+using MoviesCatalog.Infra.Data.Identity;
 using MoviesCatalog.Infra.Data.Repositories;
 
 namespace MoviesCatalog.Infra.IoC;
@@ -20,16 +23,27 @@ public static class DependencyInjection
             options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")
                 , b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
 
+        services.AddIdentity<ApplicationUser, IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+
+        services.ConfigureApplicationCookie(options =>
+            options.AccessDeniedPath = "/Account/Login");
+        
         services.AddScoped<ICategoryRepository, CategoryRepository>();
         services.AddScoped<IMovieRepository, MovieRepository>();
 
         services.AddScoped<ICategoryService, CategoryService>();
         services.AddScoped<IMovieService, MovieService>();
-
+        
+        services.AddScoped<IAuthenticate, AuthenticateService>();
+        services.AddScoped<ISeedUserRoleInitial, SeedUserRoleInitial>();
+        
         services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
 
         var myhandlers = AppDomain.CurrentDomain.Load("MoviesCatalog.Application");
         services.AddMediatR(myhandlers);
+        
         
         return services;
     }
