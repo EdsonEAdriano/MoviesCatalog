ae9cef06c86f2951f3db2ba5fb3d4bdafd5e2e94
Edsu <edsoneurides0705@gmail.com>
2024-02-06 23:26:53 -0300

    Controlador de contas

---
+++ MoviesCatalog.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MoviesCatalog.Domain.Account;
+using MoviesCatalog.WebUI.ViewModels;
+
+namespace MoviesCatalog.WebUI.Controllers;
+
+public class AccountController : Controller
+{
+    private readonly IAuthenticate _auth;
+
+    public AccountController(IAuthenticate auth)
+    {
+        _auth = auth;
+    }
+    
+    [HttpGet]
+    [AllowAnonymous]
+    public IActionResult Login(string returnUrl)
+    {
+        return View(new LoginViewModel()
+        {
+            ReturnUrl = returnUrl
+        });
+    }
+
+    [HttpPost]
+    [AllowAnonymous]
+    public async Task<IActionResult> Login(LoginViewModel model)
+    {
+        var result = await _auth.Authenticate(model.Email, model.Password);
+
+        if (result)
+        {
+            if (string.IsNullOrEmpty(model.ReturnUrl))
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return Redirect(model.ReturnUrl);
+        }
+        else
+        {
+            ModelState.AddModelError(string.Empty, "Invalid login attempt. (password must be strong).");
+            return View(model);
+        }
+    }
+
+
+    [HttpGet]
+    [AllowAnonymous]
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+
+    [HttpPost]
+    [AllowAnonymous]
+    public async Task<IActionResult> Register(RegisterViewModel model)
+    {
+        var result = await _auth.RegisterUser(model.Email, model.Password);
+
+        if (result)
+        {
+            return Redirect("/");
+        }
+        else
+        {
+            ModelState.AddModelError(string.Empty, "Invalid register attempt (password must be strong).");
+            return View(model);
+        }
+    }
+
+    public async Task<IActionResult> Logout()
+    {
+        await _auth.Logout();
+        return Redirect("/Account/Login");
+    }
+}
