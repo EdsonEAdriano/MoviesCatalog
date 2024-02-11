8d68f357c6576fbf2f9be3c6a7fbbd0ed0c21ffb
Edsu <edsoneurides0705@gmail.com>
2024-02-10 21:05:52 -0300

    Criação das dependencias para API

---
+++ MoviesCatalog.Infra.IoC/DependencyInjectionAPI.cs
@@ -0,0 +1,46 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MoviesCatalog.Application.Interfaces;
+using MoviesCatalog.Application.Mappings;
+using MoviesCatalog.Application.Services;
+using MoviesCatalog.Domain.Account;
+using MoviesCatalog.Domain.Interfaces;
+using MoviesCatalog.Infra.Data.Context;
+using MoviesCatalog.Infra.Data.Identity;
+using MoviesCatalog.Infra.Data.Repositories;
+
+namespace MoviesCatalog.Infra.IoC;
+
+public static class DependencyInjectionAPI
+{   
+    public static IServiceCollection AddInfrastructureAPI(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")
+                , b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+        services.AddIdentity<ApplicationUser, IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+        
+        services.AddScoped<ICategoryRepository, CategoryRepository>();
+        services.AddScoped<IMovieRepository, MovieRepository>();
+
+        services.AddScoped<ICategoryService, CategoryService>();
+        services.AddScoped<IMovieService, MovieService>();
+        
+        services.AddScoped<IAuthenticate, AuthenticateService>();
+        
+        services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+
+        var myhandlers = AppDomain.CurrentDomain.Load("MoviesCatalog.Application");
+        services.AddMediatR(myhandlers);
+        
+        
+        return services;
+    }
+}
