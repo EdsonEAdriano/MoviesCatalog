f1eaa8cba6db154fa8e088e7636b2b64fbe7f7bd
Edsu <edsoneurides0705@gmail.com>
2024-02-12 22:43:57 -0300

    Adicionando as configurações do JWT e Swagger no projeto WEB API

--- MoviesCatalog.WebAPI/Program.cs
+++ MoviesCatalog.WebAPI/Program.cs
@@ -7,9 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 builder.Services.AddInfrastructureAPI(builder.Configuration);
+builder.Services.AddInfrastructureJWT(builder.Configuration);
+builder.Services.AddInfrastructureSwagger();
 
 var app = builder.Build();
 
@@ -22,6 +23,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
