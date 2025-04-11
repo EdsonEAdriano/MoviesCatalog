b3391841a4f4ee2d570b039e3dded22af61a2b55
Edsu <79771919+EdsonEAdriano@users.noreply.github.com>
2025-04-11 15:14:50 -0300

    Update Program.cs

--- MoviesCatalog.WebAPI/Program.cs
+++ MoviesCatalog.WebAPI/Program.cs
@@ -8,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 
+// Set Dependency Injection
 builder.Services.AddInfrastructureAPI(builder.Configuration);
 builder.Services.AddInfrastructureJWT(builder.Configuration);
 builder.Services.AddInfrastructureSwagger();
